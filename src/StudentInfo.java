0a8f8d51a0d8c0c4385ba43d1aa31011df56fe61
Sara-kr <saravananofficial18@gmail.com>
2025-02-10 23:09:56 +0530

    Second Commit

---
+++ src/StudentInfo.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+public class StudentInfo {
+
+  private List<Student> students = new ArrayList<>();
+
+  public void addStudent(Student student){
+        students.add(student);
+    System.out.println("Student add successfully");
+  }
+  public void viewStudent(){
+      if(students.isEmpty()){
+          System.out.println("No Student Found");
+          return;
+      }
+      else {
+          for (Student stu : students) {
+              System.out.println(stu);
+          }
+      }
+  }
+  public void searchStudent(int id){
+      for(Student student : students){
+          if (student.getId()==id){
+              System.out.println("Student found " + student);
+          }
+          else {
+              System.out.println("Not Found");
+          }
+      }
+  }
+  public void updateStudent(int id,String name,int age,String grade,String email){
+      for (Student student : students){
+          if(student.getId()==id){
+              student.setName(name);
+              student.setAge(age);
+              student.setGrade(grade);
+              student.setEmail(email);
+              System.out.println("Updated Successfully");
+              System.out.println(student);
+          }
+      }
+      System.out.println("ID is not Found");
+  }
+  public void deleteStudent(int id){
+      Iterator<Student> iterator = students.iterator();
+      while (iterator.hasNext()){
+          if (iterator.next().getId()==id){
+              iterator.remove();
+              System.out.println("Removed Successfully");
+          }
+      }
+      System.out.println("Not Found");
+
+  }
+}
---
+++ src/StudentMain.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StudentMain {
+    public static void main(String[] args) {
+        StudentInfo studentInfo = new StudentInfo();
+        int id=0;
+        boolean check = true;
+        while (check){
+            System.out.println("1. Add Student");
+            System.out.println("2. View Students");
+            System.out.println("3. Search Student");
+            System.out.println("4. Update Student");
+            System.out.println("5. Delete Student");
+            System.out.println("6. Exit");
+
+            Scanner sc = new Scanner(System.in);
+            System.out.print("User Choice: ");
+            int userChoice = sc.nextInt();
+            sc.nextLine();
+            id++;
+            switch (userChoice){
+                case 1 :
+                    System.out.println("Enter the name : ");
+                    String name = sc.nextLine();
+
+                    System.out.println("Enter the Age");
+                    int age = sc.nextInt();
+                    sc.nextLine();
+                    System.out.println(" Enter the Grade");
+                    String grade = sc.nextLine();
+
+                    System.out.println("Enter the E-Mail");
+                    String email = sc.nextLine();
+                   studentInfo.addStudent(new Student(id,name,age,grade,email));
+                   break;
+                case 2:
+                    studentInfo.viewStudent();
+                    break;
+                case 3:
+                    System.out.println("Enter the ID to Search");
+                    int Id = sc.nextInt();
+                    studentInfo.searchStudent(Id);
+                    break;
+                case 4:
+                    System.out.println("Enter the ID to update");
+                    int newId = sc.nextInt();
+                    sc.nextLine();
+                    System.out.println("Enter the New Name");
+                    String newName = sc.nextLine();
+                    System.out.println("Enter the New Age");
+                    int newAge = sc.nextInt();
+                    sc.nextLine();
+                    System.out.println("Enter the New Grade ");
+                    String newGrade = sc.nextLine();
+                    System.out.println("Enter the E-Mail");
+                    String newMail = sc.nextLine();
+
+                    studentInfo.updateStudent(newId,newName,newAge,newGrade,newMail);
+                    break;
+                case 5:
+                    System.out.println("Enter the ID to Delete");
+                    int delId=sc.nextInt();
+                    studentInfo.deleteStudent(delId);
+                    break;
+                case 6:
+                    System.out.println("Exiting.....");
+                    check=false;
+                    break;
+
+                default:
+                    System.out.println("Invalid ! Try Again");
+            }
+        }
+    }
+}
